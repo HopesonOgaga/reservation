c01336e118f60cd2f9d06b049050d871a3f979ab
hopeson <hopesonogaga@gmail.com>
2024-10-08 22:16:27 +0100

    dateinfo

--- src/components/home/datedata/index.js
+++ src/components/home/datedata/index.js
@@ -7,32 +7,72 @@ export default function DateInfo() {
   const [adult, setAdult] = useState(Number);
 
   return (
-    <section>
-      <div>
-        <div>
-          <label htmlFor="adult"></label>
-          <select className="" id="adult" name="adult">
-            {/* Default option */}
-            <option value="" className="fonte-semibold text-lg capitalize">Select Adult</option>
-            {/* Dynamically create options from the adult array */}
-            {adult.map((adultValue, index) => (
-              <option key={index} value={adultValue} className="capitalize font-light ">
-                {adultValue} <p >adults</p>
+    <section className="flex justify-center w-full h-full">
+      <div className=" w-[60vw] h-[12vh] bg-white rounded-md shadow-md flex flex-col justify-center items-center ">
+        <div className="flex justify-around w-full h-full items-center  ">
+          <div>
+            <label htmlFor="adult"></label>
+            <select className="" id="adult" name="adult">
+              {/* Default option */}
+              <option value="" className="font-semibold text-lg capitalize">
+                Select Adult
               </option>
-            ))}
-          </select>
-        </div>
-        <div>
+              {/* Dynamically create options from the adult array */}
+              {adult.map((adultValue, index) => (
+                <option
+                  key={index}
+                  value={adultValue}
+                  className="capitalize font-light "
+                >
+                  {adultValue} <p>adults</p>
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="border border-r-2 border-blue-400 h-14"></div>
+          {/*  */}
+          <div>
+            <label htmlFor="adult"></label>
+            <select className="" id="adult" name="adult">
+              {/* Default option */}
+              <option value="" className="font-semibold text-lg capitalize">
+                Select Adult
+              </option>
+              {/* Dynamically create options from the adult array */}
+              {adult.map((adultValue, index) => (
+                <option
+                  key={index}
+                  value={adultValue}
+                  className="capitalize font-light "
+                >
+                  {adultValue} <p>adults</p>
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="border border-r-2 border-blue-400 h-14"></div>
+          <div>
             <label for=""></label>
-             <select className="" id="child" name="child">
-                <option value="" className="fonte-semibold text-lg capitalize">Select Children</option>
-                  {/* Dynamically create options from the adult array */}
-                {adult.map((childvalue, index)=>(
-                    <option key={index} value={childvalue} className="capitalize font-light ">
-                        {childvalue} <p>child</p>
-                    </option>
-                ))}
-             </select>
+            <select className="" id="child" name="child">
+              <option value="" className="font-semibold text-lg capitalize">
+                Select Children
+              </option>
+              {/* Dynamically create options from the adult array */}
+              {adult.map((childvalue, index) => (
+                <option
+                  key={index}
+                  value={childvalue}
+                  className="capitalize font-light "
+                >
+                  {childvalue} <p>child</p>
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="border border-r-2 border-blue-400 h-14"></div>
+          <div className="">
+            <button className="flex gap-4 p-2 justify-center bg-blue-400 capitalize text-lg text-white font-medium rounded-lg shadow-md w-32 h-12 hover:bg-blue-800 active:bg-red-200"><img className="w-6 " src="/image/loupe.png" alt=""></img>search</button>
+          </div>
         </div>
       </div>
     </section>
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Service from "./servicesection/services";
 import Header from "../header";
+import DateInfo from "./datedata";
 
 export default function Home() {
   return <section className="mr-4 ml-4 ">
@@ -9,6 +10,11 @@ export default function Home() {
       <p className="text-4xl">welcome to the <span className="text-sky-500 uppercase font-semibold ">hopeson</span> </p>
       <div className="border border-b-2 bg-black w-24 rounded-full shadow-sm"></div>
     </div>
+    <DateInfo/>
+    {/* about */}
+    <section>
+      
+    </section>
     <Service></Service>
     {/* slider willl be here */}
     <div>
